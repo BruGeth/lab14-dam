a56915c825a1b2d07b0014535d995fc152d8aa1f
BruGeth <g15bru03@hotmail.com>
2025-11-14 21:25:21 -0500

    feat: agregar manejo de notificaciones con expo-notifications

--- App.js
+++ App.js
@@ -1,5 +1,20 @@
+import * as Notificacions from 'expo-notifications';
+import * as Device from 'expo-device';
+import { Alert } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+Notificacions.setNotificationHandler({
+  handleNotification: async () => ({
+    // shouldShowAlert: true -> deprecated
+    // Use shouldShowBanner and shouldShowList instead
+    shouldShowBanner: true,
+    shouldShowList: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
   return (
