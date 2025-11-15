9f8d617e0d7a00f5a6c60e9b1ed2badfa4287e40
BruGeth <g15bru03@hotmail.com>
2025-11-14 21:27:14 -0500

    chore: eliminar importación no utilizada de StatusBar

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import * as Notificacions from 'expo-notifications';
 import * as Device from 'expo-device';
 import { Alert } from 'react-native';
 
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 Notificacions.setNotificationHandler({
@@ -20,7 +19,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
